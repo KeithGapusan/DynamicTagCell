bb5d94fc892d5b8ee88278bfb44ea508f755311e
Keith <krsgapusan@gmail.com>
2019-05-09 19:48:54 +0800

    Initial commit

---
+++ DynamicTagCell/DynamicTagCell/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DynamicTagCell
+//
+//  Created by Keith Randell Gapusan on 09/05/2019.
+//  Copyright © 2019 Keith Randell Gapusan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
